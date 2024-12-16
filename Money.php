947228974fd8b22d353c05099347cafba2a8453c
colin-lallauret <colinlallauret1@gmail.com>
2024-12-16 15:06:46 +0100

    second commit + remove yml

--- Money.php
+++ Money.php
@@ -1,7 +1,17 @@
 <?php
 
 class Money {
-    
+    private $amount;
+    private $currency;
+
+    public function __construct($amount, $currency) {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    public function __toString() {
+        return $this->amount . ' ' . $this->currency;
+    }
 }
 
 ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require( 'Money.php');
+
+$money = new Money(42, 'EUR');
+echo $money;
+
+?>
