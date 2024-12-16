0434842f8d3083626369c7578e5bca8263292046
colin-lallauret <colinlallauret1@gmail.com>
2024-12-16 16:01:04 +0100

    Step 3

--- Money.php
+++ Money.php
@@ -1,17 +1,26 @@
 <?php
 
-class Money {
+class Money
+{
     private $amount;
     private $currency;
 
-    public function __construct($amount, $currency) {
+    public function __construct($amount, $currency)
+    {
         $this->amount = $amount;
         $this->currency = $currency;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return $this->amount . ' ' . $this->currency;
     }
-}
 
-?>
+    public function add(Money $that)
+    {
+        if ($this->currency !== $that->currency) {
+            throw new Exception("Devises différentes");
+        }
+        return new Money($this->amount + $that->amount, $this->currency);
+    }
+}
